847056cb4d1d787b96b0d27af37b1564cf18d7b8
antagomir <leo.lahti@iki.fi>
2015-02-11 20:13:33 +0100

    ok

--- tests/testthat/test_pagepolish.R
+++ tests/testthat/test_pagepolish.R
@@ -1,6 +1,13 @@
 context("Page count")
 
 test_that("page count is correct", {
-  expect_equal(unlist(polish_pages(c("1"))$estimated.pages), 1)
+  expect_equal(polish_pages("3")$estimated.pages[[1]], 3)
+  expect_equal(polish_pages("[3]")$estimated.pages[[1]], 3)
+  expect_equal(polish_pages("iii")$estimated.pages[[1]], 3)
+  expect_equal(polish_pages("[1], 3-70, [2]")$estimated.pages[[1]], 70)
+  expect_equal(polish_pages("8,[28],37-88p.")$estimated.pages[[1]], 88)
+  expect_equal(polish_pages("[2],1107-1217,[1]p.")$estimated.pages[[1]], 112)
+  expect_equal(polish_pages("505-508")$estimated.pages[[1]], 4)
 })
+
 
