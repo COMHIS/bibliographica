d462c897296d682c7184a44b9da520892a54caa8
antagomir <leo.lahti@iki.fi>
2015-02-14 15:00:46 +0100

    ok

--- tests/testthat/test_pagepolish.R
+++ tests/testthat/test_pagepolish.R
@@ -77,3 +77,30 @@ test_that("page count is correct", {
 })
 
 
+# Summary of unit testing with testthat
+# http://r-pkgs.had.co.nz/tests.html
+
+# Create testthat directory and scripts:
+# devtools::use_testthat()
+
+# Test your package with 
+# Ctrl/Cmd + Shift + T or devtools::test().
+
+# Test options
+#    is_true: Does the expression evaluate to TRUE?
+#    is_false: Does the expression evaluate to FALSE?
+#    is_a: Did the object inherit from a specified class?
+#    equals: Is the expression equal within numerical tolerance to your expected value?
+#    is_equivalent_to: Is the object equal up to attributes to your expected value?
+#    is_identical_to: Is the object exactly equal to your expected value?
+#    matches: Does a string match the specified regular expression?
+#    prints_text: Does the text that’s printed match the specified regular expression?
+#    throws_error: Does the expression raise an error?
+#    takes_less_than: Does the expression take less than a specified number of seconds to run?
+
+#test_that("floor_date works for different units", {
+#  base <- as.POSIXct("2009-08-03 12:01:59.23", tz = "UTC")
+#  expect_equal(floor_date(base, "second"), 
+#    as.POSIXct("2009-08-03 12:01:59", tz = "UTC"))
+#  expect_equal(floor_date(base, "minute"), 
+#}})
