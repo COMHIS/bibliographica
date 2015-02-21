cf1e0a9298e8f190108c4ef25d9628ef847ee4ae
antagomir <leo.lahti@iki.fi>
2015-02-21 17:35:09 +0100

    vignette fix

--- inst/extras/build.cran.sh
+++ inst/extras/build.cran.sh
@@ -6,6 +6,6 @@
 
 /usr/bin/R CMD BATCH document.R
 /usr/bin/R CMD build ../../
-/usr/bin/R CMD check --as-cran bibliographica_0.1.10.tar.gz
-/usr/bin/R CMD INSTALL bibliographica_0.1.10.tar.gz
+/usr/bin/R CMD check --as-cran bibliographica_0.1.11.tar.gz
+/usr/bin/R CMD INSTALL bibliographica_0.1.11.tar.gz
 
