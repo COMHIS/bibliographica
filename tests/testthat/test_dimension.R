46e9b6a32022cab4afe46fb0497bb71b156f1b81
antagomir <leo.lahti@iki.fi>
2015-04-15 23:32:45 +0200

    dimension conversion tests started

--- tests/testthat/test_dimension.R
+++ tests/testthat/test_dimension.R
@@ -2,5 +2,5 @@ context("Dimension table")
 
 test_that("dimension fill works correctly", {
   dimension.table <- dimension_table()
-  #expect_equal(fill_dimensions(c(original="12to", gatherings="12to", width=13, height=NA), dimension.table)[["height"]], "19")
+  expect_equal(fill_dimensions(c(original="12to", gatherings="12to", width=13, height=NA), dimension.table)[["height"]], "18")
 })
