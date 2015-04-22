f7136bf9df37c99421b50b52c9a9acf468a221ab
antagomir <leo.lahti@iki.fi>
2015-04-22 22:22:52 +0200

    tutorial update

--- inst/extras/build.cran.sh
+++ inst/extras/build.cran.sh
@@ -7,6 +7,6 @@
 /usr/local/bin/R CMD BATCH document.R
 /usr/local/bin/R CMD build ../../ --no-build-vignettes
 #/usr/local/bin/R CMD check --as-cran bibliographica_0.1.22.tar.gz
-/usr/local/bin/R CMD check bibliographica_0.1.29.tar.gz
-/usr/local/bin/R CMD INSTALL bibliographica_0.1.29.tar.gz
+/usr/local/bin/R CMD check bibliographica_0.1.30.tar.gz
+/usr/local/bin/R CMD INSTALL bibliographica_0.1.30.tar.gz
 
