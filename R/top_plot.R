8e38e2e0513d40636664959f0d3601c90ef4c8ff
antagomir <leo.lahti@iki.fi>
2015-06-22 23:31:47 +0200

    document size table

--- R/top_plot.R
+++ R/top_plot.R
@@ -4,6 +4,7 @@
 #' @param df Data frame, vector or factor
 #' @param field Field to show
 #' @param ntop Number of top entries to show
+#' @param color color field
 #' @return ggplot object
 #'
 #' @import ggplot2
@@ -14,13 +15,13 @@
 #' 
 #' @examples \dontrun{p <- top_plot(df, field, 50)}
 #' @keywords utilities
-top_plot <- function (df, field = NULL, ntop = NULL) {
+top_plot <- function (df, field = NULL, ntop = NULL, highlight = NULL) {
 
   x <- df
   if (is.data.frame(df)) {
     x <- df[[field]]
   }
-  
+
   tab <- rev(sort(table(x)))
   dfs <- data.frame(list(names = names(tab), count = tab))
   dfs <- dfs[dfs$count > 0,]
@@ -34,8 +35,14 @@ top_plot <- function (df, field = NULL, ntop = NULL) {
   dfs <- dfs[1:ntop,] # Pick top-n items
   dfs$names <- droplevels(factor(dfs$names, levels = rev(dfs$names)))
 
+  dfs$color <- rep("black", nrow(dfs))
+  if (!is.null(highlight)) {
+    dfs$color <- rep("darkgray", nrow(dfs))
+    dfs$color[dfs$names %in% highlight] <- "red"
+  }
+
   theme_set(theme_bw(15))
-  p <- ggplot(dfs, aes(x = names, y = count))
+  p <- ggplot(dfs, aes(x = names, y = count, fill = color))
   p <- p + geom_bar(stat = "identity")
   p <- p + coord_flip()
   p <- p + ylab(field) + xlab("")
