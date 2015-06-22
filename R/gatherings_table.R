448aadb57aa030794827252459d10e53c3489128
antagomir <leo.lahti@iki.fi>
2015-06-22 23:37:16 +0200

    Added function to read gatherings

---
+++ R/gatherings_table.R
@@ -0,0 +1,23 @@
+#' @title gatherings_table
+#' @description Document gatherings names mapping table
+#'
+#' @param ... Arguments to be passed
+#' @return Document gatherings table
+#'
+#' @export
+#' 
+#' @author Leo Lahti \email{leo.lahti@@iki.fi}
+#' @references See citation("bibliographica")
+#' 
+#' @examples gatherings_table()
+#' @keywords utilities
+gatherings_table <- function (...) {
+  f <- system.file("extdata/document_size_abbreviations.csv", package = "bibliographica")
+  dd <- read.csv(f, header = TRUE, sep = "\t")
+  dd <- apply(dd, 2, as.character)
+  dd[dd == ""] <- NA
+  dd <- as.data.frame(dd, stringsAsFactors = FALSE)
+
+  dd
+
+}
