83bf18de5e2f9544e704a5aadaf26e3de0d9b6f1
antagomir <leo.lahti@iki.fi>
2015-07-18 01:13:32 +0100

    ok

---
+++ R/harmonize_field_names.R
@@ -0,0 +1,21 @@
+#' Convert field codes to names
+#' 
+#' @param x a vector of field codes
+#' @return Vector of field names
+#'
+#' @export
+#' 
+#' @author Leo Lahti \email{leo.lahti@@iki.fi}
+#' @references See citation("bibliographica")
+#' 
+#' @examples x2 <- harmonize_field_names("041a")
+#' @keywords utilities
+harmonize_field_names <- function (x) {
+  
+  # Get printing terms from a table
+  f <- system.file("extdata/fieldnames.csv", package = "bibliographica")
+  map <- read.csv(f, sep = "\t")
+  map[match(x, map$field), "name"]
+  
+}
+
---
+++ R/remove_sl.R
@@ -0,0 +1,31 @@
+#' Remove s.l etc. clauses 
+#'
+#' @param x A character vector
+#' @param terms Terms to be removed (a character vector). Optional.
+#'
+#' @return Polished vector
+#'
+#' @export
+#' 
+#' @author Leo Lahti \email{leo.lahti@@iki.fi}
+#' @references See citation("bibliographica")
+#' 
+#' @examples x2 <- remove_sl(c("s.l.", "London"))
+#' @keywords utilities
+remove_sl <- function (x, terms = NULL) {
+
+  # Get printing terms from a table
+  if (is.null(terms)) {
+    f <- system.file("extdata/sl.csv", package = "bibliographica") 
+    terms <- as.character(read.csv(f)[,1])
+  }
+
+  x <- remove_terms(x, terms, include.lowercase = TRUE)
+
+  x
+
+}
+
+
+
+
