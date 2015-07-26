e15105bb1ba702bc00071d7bca49cb639c824013
antagomir <leo.lahti@iki.fi>
2015-07-26 21:13:30 +0200

    ok

--- inst/extras/build.cran.sh
+++ inst/extras/build.cran.sh
@@ -5,8 +5,8 @@
 # http://r-pkgs.had.co.nz/man.html
 
 /usr/local/bin/R CMD BATCH document.R
-/usr/local/bin/R CMD build ../../ --no-build-vignettes --no-tests
+/usr/local/bin/R CMD build ../../ --no-build-vignettes #--no-tests
 #/usr/local/bin/R CMD check --as-cran bibliographica_0.1.22.tar.gz
-/usr/local/bin/R CMD check bibliographica_0.1.35.tar.gz --no-build-vignettes --no-tests
+/usr/local/bin/R CMD check bibliographica_0.1.35.tar.gz --no-build-vignettes #--no-tests
 /usr/local/bin/R CMD INSTALL bibliographica_0.1.35.tar.gz
 
