190e64d3fd70d0c487b1470365105ef8b870bedb
hegroiva <hege.roivainen@gmail.com>
2015-10-26 13:47:15 +0200

    Added polish_title
    Added polish_title

---
+++ R/polish_title.R
@@ -0,0 +1,49 @@
+#' @title polish_title
+#' @description Polish title field
+#'
+#' @param x Vector of titles
+#' @return Vector of titles polished
+#'
+#' @export
+#' @details Remove ending commas, periods, spaces and parentheses, 
+#' 	    starting prepositions etc.
+#' 
+#' @author Leo Lahti \email{leo.lahti@@iki.fi}
+#' @references See citation("estc")
+#' 
+#' @examples \dontrun{x2 <- polish_title(x)}
+#' @keywords utilities
+polish_title <- function (x) {
+
+  x <- as.character(x)
+
+  x <- gsub("\\,$", "", x)
+  x <- gsub("\\.$", "", x)
+
+  x <- gsub(":$", "", x)
+  x <- gsub(";$", "", x)
+
+  x <- gsub("^a ", "^A ", x)
+  x <- gsub("^\\[A\\] ", "^A ", x)
+  x <- gsub("^\\[a\\] ", "^A ", x)
+  x <- gsub("^\\[d\\] ", "^D ", x)
+  x <- gsub("^\\[D\\] ", "^D ", x)
+  x <- gsub("^the ", "^The ", x)
+  x <- gsub("\\. A  ", ", a ", x)
+  x <- gsub(" TEST TEST TEST ", " ", x)
+
+  # Additions for Kungliga
+   x <- gsub("(^[^/]+)([/]+)$", "\\1", x) 
+	# Remove slashes from the end
+	# unless slashes are used as a container
+  x <- gsub("\\.$", "", x)
+  x <- gsub("[ ]+$", "", x)
+  # Additions end here
+
+  x <- gsub("\\]$", "", x)
+  x <- gsub("^\\[", "", x)
+  x <- gsub("\\)$", "", x)
+  x <- gsub("^\\(", "", x)
+
+  x
+}
