6127ab2f36f99c4f9d71ac7598b4cfd82a7a013c
antagomir <leo.lahti@iki.fi>
2015-10-30 15:25:12 -0200

    ok

--- tests/testthat/test_dimension.R
+++ tests/testthat/test_dimension.R
@@ -80,7 +80,7 @@ test_that("dimension polish works correctly", {
   expect_equal(as.character(polish_dimensions("(4to and 8vo)")$gatherings), "NA")
   expect_equal(as.character(polish_dimensions("(fol)")$gatherings), "2fo")
   expect_equal(as.character(polish_dimensions("2\u00b0; 32 x 20 cm.")$gatherings), "2fo")
-  expect_equal(as.character(polish_dimensions("Broadside. 35 x 24 cm.")$gatherings), "bs")
+  expect_equal(as.character(polish_dimensions("Broadside. 35 x 24 cm.")$gatherings), "1to")
   expect_equal(as.character(polish_dimensions("32 cm. (fol))")$gatherings), "2fo")
   expect_equal(as.character(polish_dimensions("20 cm. (4to & 8vo)")$gatherings), "NA")
 
