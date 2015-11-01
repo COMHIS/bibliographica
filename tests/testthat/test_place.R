5c0a456276da4921e27c4b9661bb6a10aaf41676
antagomir <leo.lahti@iki.fi>
2015-11-01 00:45:03 -0200

    ok

--- tests/testthat/test_place.R
+++ tests/testthat/test_place.R
@@ -1,6 +1,10 @@
 context("Harmonize place names")
 
 test_that("Places are harmonized correctly", {
+
+  expect_equal(polish_place("Te Philadelphia"), "Philadelphia Pa")
+  expect_equal(polish_place("Imprinted in Aberdene"), "Aberdeen")
+
   expect_equal(polish_place("Tryckt i Vpsala"), "Uppsala")
   expect_equal(polish_place("A Londres : [s.n.]"), "London")
   expect_equal(polish_place("A Londres"), "London")
