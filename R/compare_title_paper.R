808bf7b675b99f226aefc01e6abf5f26f88d7c96
antagomir <leo.lahti@iki.fi>
2016-01-20 21:27:35 +0100

    ok

---
+++ R/compare_title_paper.R
@@ -0,0 +1,50 @@
+#' @title Compare title count and paper consumption
+#' @description Compare title count and paper consumption for selected field
+#' @param x data frame
+#' @param field Field to analyze
+#' @param selected Limit the analysis on selected entries
+#' @param plot.mode "text" or "point"
+#' @return List:
+#' \itemize{
+#'   \item{plot}{ggplot object}
+#'   \item{table}{summary table}
+#' }
+#' @export
+#' @author Leo Lahti \email{leo.lahti@@iki.fi}
+#' @references See citation("bibliographica")
+#' @examples \dontrun{compare_title_paper(df, "author")}
+#' @keywords utilities
+compare_title_paper <- function (x, field, selected = NULL, plot.mode = "text") {
+
+  paper.consumption.km2 <- publication_decade <- titles <- paper <- NULL
+
+  x$field <- x[[field]]
+
+  # Nothing selected - take all
+  if (is.null(selected)) {selected <- unique(x$field)}
+
+  x <- x %>% group_by(field) %>%
+            filter(!is.na(field)) %>%
+            filter(field %in% selected) %>%              
+            summarize(titles = n(),
+            paper = sum(paper.consumption.km2, na.rm = T))
+
+
+  # Prepare the plot
+  p <- ggplot(x, aes(x = titles, y = paper)) +
+    xlab("Title count") + ylab("Paper consumption (km2)") + 
+    ggtitle(paste("Title count versus paper (", field, ")")) +
+    scale_x_log10() + scale_y_log10() 
+
+  if (plot.mode == "text") {
+    p <- p + geom_text(aes(label = field), size = 5) 
+  } else if (plot.mode == "point") {
+    p <- p + geom_point(size = 2)
+  }
+
+  colnames(x)[[1]] <- field
+
+  list(plot = p, table = x)
+
+}
+
---
+++ R/paper_timeline.R
@@ -0,0 +1,36 @@
+#' @title Paper consumption timeline
+#' @description Compare paper consumption among selected groups
+#' @param x data frame
+#' @param field Field indicating the groups to compare
+#' @param nmin Include only entries with at least nmin occurrences
+#' @return List:
+#' \itemize{
+#'   \item{plot}{ggplot object}
+#'   \item{table}{summary table}
+#' }
+#' @export
+#' @author Leo Lahti \email{leo.lahti@@iki.fi}
+#' @references See citation("bibliographica")
+#' @examples \dontrun{paper_timeline(df, "gatherings")}
+#' @keywords utilities
+paper_timeline <- function (x, field, nmin = 0) {
+
+  paper.consumption.km2 <- publication_decade <- NULL
+
+  x$field <- x[[field]]
+
+  df2 <- x %>% group_by(publication_decade, field) %>%
+     summarize(paper.consumption.km2 = sum(paper.consumption.km2, na.rm = TRUE), n = n())
+
+  # Remove entries with too few occurrences
+  df2 <- df2 %>% filter(field %in% setdiff(names(which(table(df2$field) >= nmin)), "NA")) 
+	      
+  p <- ggplot(df2, aes(y = paper.consumption.km2, x = publication_decade, shape = field, linetype = field)) +
+     geom_point(size = 4) +
+     geom_smooth(method = "loess", size = 1, color = "black") +
+     ggtitle(paste("Paper consumption in time by ", field)) +
+     xlab("Year") + ylab("Paper consumption (km2)") +
+     guides(linetype = guide_legend(keywidth = 5), shape = guide_legend(keywidth = 5)) +
+     ggtitle("Paper consumption")
+   list(plot = p, table = df2)
+}
