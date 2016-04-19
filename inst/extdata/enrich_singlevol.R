1ad0145fd47fa4015f57d186f2fe02a19d694b17
antagomir <leo.lahti@iki.fi>
2016-04-19 21:02:48 +0200

    ok

--- inst/extdata/enrich_singlevol.R
+++
@@ -1,6 +0,0 @@
-g <- as.character(df.preprocessed$gatherings[inds])
-print("Pick the estimated page counts per vol separately for each doc size")
-inds2 = match(g, as.character(mean.pagecounts.univol$doc.dimension))
-pages.per.vol <- mean.pagecounts.univol[inds2, ]$median.pages.singlevol
-print("Add estimated total page counts for all docs")
-df.preprocessed[inds, "pagecount"] <- 1 * pages.per.vol
