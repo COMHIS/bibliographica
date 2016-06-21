208a3ade7f5318efd5610542b07d04918a1c2378
Leo Lahti <leo.lahti@iki.fi>
2016-06-21 15:57:56 +0200

    ok

--- R/top.R
+++ R/top.R
@@ -4,13 +4,14 @@
 #' @param field Field or column to check for a data.frame or matrix
 #' @param n Number of top entries to show
 #' @param output Output format: vector or data.frame
+#' @param round Optional rounding
 #' @return Vector of top counts, named by the corresponding entries
 #' @export
 #' @author Leo Lahti \email{leo.lahti@@iki.fi}
 #' @references See citation("bibliographica")
 #' @examples \dontrun{p <- top(df, field, 10)}
 #' @keywords utilities
-top <- function (x, field = NULL, n = NULL, output = "vector") {
+top <- function (x, field = NULL, n = NULL, output = "vector", round = NULL) {
 
   if (is.vector(x)) {
     s <- rev(sort(table(x)))
@@ -30,6 +31,11 @@ top <- function (x, field = NULL, n = NULL, output = "vector") {
 
   if (output == "data.frame") {
     s <- data_frame(name = names(s), n = unname(s), fraction = 100*unname(s)/N)
+    colnames(s) <- c(field, "Entries (N)", "Fraction (%)")
+    if (!is.null(round)) {
+      pubc[,3] = round(pubc[,3], round)
+    }
+ 
   }
 
   s
