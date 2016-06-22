bc1a3c5157c7d37b5428367cd773c2704b7554a6
hegroiva <hege.roivainen@gmail.com>
2016-06-22 12:26:21 +0300

    Moved some tests into csv-files
    Moved some tests into csv-files

--- tests/testthat/test_dimension.R
+++ tests/testthat/test_dimension.R
@@ -20,128 +20,41 @@ test_that("dimension tables are valid", {
 
 test_that("dimension polish works correctly", {
 
-  # escaped unicode character
-  # \u00b0 = astemerkki ⁰
-
-  expect_equal(as.character(polish_dimensions("23 x 31.3 cm. plate 25 x 33.6 cm.")$width), "23")
-  expect_equal(as.character(polish_dimensions("23 x 31.3 cm. plate 25 x 33.6 cm.")$height), "31.3")  
-
-  expect_equal(as.character(polish_dimensions("2\u00b0(3?)")$gatherings), "2fo")
-  expect_equal(as.character(polish_dimensions("2fo(3?).")$gatherings), "2fo")
-
-  expect_equal(as.character(polish_dimensions("16:0")$gatherings), "16mo")
-  expect_equal(as.character(polish_dimensions("6:o")$gatherings), "6to")
-  
-  expect_equal(as.character(polish_dimensions("46 cm(2\u00b0)")$gatherings), "2fo")
-  expect_equal(as.character(polish_dimensions("29-40 cm. (4\u00b0; 2\u00b0)")$gatherings), "NA")
-  expect_equal(as.character(polish_dimensions("4\u00b0. '")$gatherings), "4to")
-  expect_equal(as.character(polish_dimensions("2\u00b0 (2 half-sheets)")$gatherings), "2fo")
-  expect_equal(as.character(polish_dimensions("1/2\u00b0; 2\u00b0.")$gatherings), "2fo")
-  expect_equal(as.character(polish_dimensions("2\u00b0; 1/2\u00b0.")$gatherings), "2fo")
-  expect_equal(as.character(polish_dimensions("1/2\u00b0? (34.5 x 16 cm.)")$gatherings), "2fo")
-  expect_equal(as.character(polish_dimensions("1/2\u00b0; 35.5 x 22.5 cm.")$gatherings), "2fo")
-  expect_equal(as.character(polish_dimensions("18 cm. (8vo; horizontal chain lines)")$gatherings), "8vo")
-  expect_equal(as.character(polish_dimensions("1/2\u00b0(?)")$gatherings), "2fo")
-  expect_equal(as.character(polish_dimensions("40; 34; 42 cm (2\u00b0; 1/2\u00b0; 2\u00b0)")$gatherings), "2fo")
-  expect_equal(as.character(polish_dimensions("obl4\u00b0.")$gatherings), "4to")
-  expect_equal(as.character(polish_dimensions("51-34 cm (2\u00b0-obl. 2\u00b0)")$gatherings), "2fo")
-  expect_equal(as.character(polish_dimensions("49 cm (1/2\u00b0, 2\u00b0)")$gatherings), "2fo")
-  expect_equal(as.character(polish_dimensions("42 cm.(2\u00b0)")$gatherings), "2fo")
-  expect_equal(as.character(polish_dimensions("20 cm. (4to?)")$gatherings), "4to")
-
-  expect_equal(as.character(polish_dimensions("NA", fill = FALSE, dimtab = NULL)$gatherings), "NA")
-  expect_equal(as.character(polish_dimensions("2fo.;2fo.")$gatherings), "2fo")
-  expect_equal(as.character(polish_dimensions("2fo")$gatherings), "2fo")
-  expect_equal(as.character(polish_dimensions("4to;2fo")$gatherings), "NA")
-  expect_equal(as.character(polish_dimensions("2fo;1to")$gatherings), "NA")
-  expect_equal(as.character(polish_dimensions("4to-2fo")$gatherings), "NA")
-  expect_equal(as.character(polish_dimensions("cm.4to")$gatherings), "4to")
-  expect_equal(as.character(polish_dimensions("12mo.f")$gatherings), "12mo")
-  expect_equal(as.character(polish_dimensions("4to.;4to")$gatherings), "4to")
-  expect_equal(as.character(polish_dimensions("4to-4to")$gatherings), "4to")
-  
-  expect_equal(as.character(polish_dimensions("4\u00b0, 2\u00b0 and 1\u00b0.")$gatherings), "NA")
-  expect_equal(as.character(polish_dimensions("31-44 cm (4\u00b0; 2\u00b0)")$gatherings), "NA")
-  expect_equal(as.character(polish_dimensions("4\u00b0 and 8\u00b0.")$gatherings), "NA")
-  expect_equal(as.character(polish_dimensions("4\u00b0; 2\u00b0.")$gatherings), "NA")
-  expect_equal(as.character(polish_dimensions("4\u00b0; sm 2\u00b0; 2\u00b0 (30-51 cm.).")$gatherings), "NA")
-  expect_equal(as.character(polish_dimensions("2\u00b0; 1\u00b0.")$gatherings), "NA")
-  expect_equal(as.character(polish_dimensions("4\u00b0 and 2\u00b0.")$gatherings), "NA")
-  expect_equal(as.character(polish_dimensions("2\u00b0; 4\u00b0.")$gatherings), "NA")
-  expect_equal(as.character(polish_dimensions("2\u00b0; later, 1\u00b0.")$gatherings), "NA")
-  expect_equal(as.character(polish_dimensions("44-55 cm (2\u00b0;1\u00b0)")$gatherings), "NA")
-
-  expect_equal(as.character(polish_dimensions("4\u00b0; 43 cm (2\u00b0)")$gatherings), "NA")
-  expect_equal(as.character(polish_dimensions("1/2.")$gatherings), "2fo")
-  expect_equal(as.character(polish_dimensions("1/2")$gatherings), "2fo")
-  expect_equal(as.character(polish_dimensions("8")$gatherings), "8vo")
-  expect_equal(as.character(polish_dimensions("8\u00b0 in 4's.")$gatherings), "8vo")
-  expect_equal(as.character(polish_dimensions("16\u00b0 in 8's.")$gatherings), "16mo")
-  expect_equal(as.character(polish_dimensions("4\u00b0 in 8's.")$gatherings), "4to")
-  expect_equal(as.character(polish_dimensions("12\u00b0 & 8\u00b0")$gatherings), "NA")
-  expect_equal(as.character(polish_dimensions("21 cm. (4to and 8vo)")$gatherings), "NA")
-  expect_equal(as.character(polish_dimensions("(4to and 8vo)")$gatherings), "NA")
-  expect_equal(as.character(polish_dimensions("(fol)")$gatherings), "2fo")
-  expect_equal(as.character(polish_dimensions("2\u00b0; 32 x 20 cm.")$gatherings), "2fo")
-  expect_equal(as.character(polish_dimensions("Broadside. 35 x 24 cm.")$gatherings), "1to")
-  expect_equal(as.character(polish_dimensions("32 cm. (fol))")$gatherings), "2fo")
-  expect_equal(as.character(polish_dimensions("20 cm. (4to & 8vo)")$gatherings), "NA")
-
-  expect_equal(as.character(polish_dimensions("1 /2⁰.")$gatherings), "2fo")
-  expect_equal(as.character(polish_dimensions("8⁸.")$gatherings), "8vo")
-  expect_equal(as.character(polish_dimensions("(12 mo)")$gatherings), "12mo")    
-
+  f <- "~/GitHub/bibliographica/tests/dimension_polish_tests.csv"
+  synonymes <- read.csv(file=f, header=TRUE, sep="\t", encoding="UTF-8")
+  for (i in 1:nrow(synonymes)) {
+    test_clause <- as.character(synonymes$clause[i])
+    test_result <- as.character(synonymes$result[i])
+    if (is.na(test_result)) {test_result <- "NA"}
+    test_field <- as.character(synonymes$field[i])
+    if ((is.na(test_field)) || (test_field =="")) {test_field <- "gatherings"}
+    if (is.na(synonymes$expected[i]) || (synonymes$expected[i] == "equal")) {
+      expect_equal(as.character(polish_dimensions(test_clause)[[test_field]]), test_result)
+    }
+  }
 
 
 })
 
 
-
 test_that("fennica dimensions", {
 
-  expect_equal(as.character(polish_dimensions("8", fill = TRUE)$gatherings), "8vo")
-  expect_equal(as.character(polish_dimensions("20.", fill = TRUE)$gatherings), "20to")  
-  expect_equal(as.character(polish_dimensions("1915.", fill = TRUE)$gatherings), "NA")
-  expect_equal(as.character(polish_dimensions("16to[7.5x5.5 cm].")$gatherings), "16mo")  
-  expect_equal(as.character(polish_dimensions("4to+")$gatherings), "4long")
-  expect_equal(as.character(polish_dimensions("8to+")$gatherings), "8long")
-  expect_equal(as.character(polish_dimensions("8to +")$gatherings), "8long")  
-  expect_equal(as.character(polish_dimensions("[1to].")$gatherings), "1to")
-  expect_equal(as.character(polish_dimensions("2.o.")$gatherings), "2fo")
-  expect_equal(as.character(polish_dimensions("[12to.")$gatherings), "12mo")
-  expect_equal(as.character(polish_dimensions("[4to.")$gatherings), "4to")
-  expect_equal(as.character(polish_dimensions("[4to].")$gatherings), "4to")
-  expect_equal(as.character(polish_dimensions("2to[1/2to].")$gatherings), "2fo")
-  expect_equal(as.character(polish_dimensions("[16to].")$gatherings), "16mo")
-  expect_equal(as.character(polish_dimensions("[[8to].")$gatherings), "8vo")
-  expect_equal(as.character(polish_dimensions("[obl.1to].")$gatherings), "1to")
-  expect_equal(as.character(polish_dimensions("[1/2to].")$gatherings), "2fo")
-  expect_equal(as.character(polish_dimensions("[8to +")$gatherings), "8long")
-  expect_equal(as.character(polish_dimensions("[16to.")$gatherings), "16mo")
-  expect_equal(as.character(polish_dimensions("[obl.2to.")$gatherings), "2fo")
-  expect_equal(as.character(polish_dimensions("[8to].")$gatherings), "8vo")
-  expect_equal(as.character(polish_dimensions("p. [28] tyhjä ;;4to")$gatherings), "4to")
-  expect_equal(as.character(polish_dimensions("4.o.")$gatherings), "4to")
-  expect_equal(as.character(polish_dimensions("8to(18 cm)")$gatherings), "8vo")
-  expect_equal(as.character(polish_dimensions("8to(18 cm)")$height), "18")  
-  expect_equal(as.character(polish_dimensions("8to(25 cm) +")$gatherings), "8long")
-  expect_equal(as.character(polish_dimensions("4to(23 cm)")$gatherings), "4to")
-  expect_equal(as.character(polish_dimensions("8to(p.urempi)")$gatherings), "8vo")
-  expect_equal(as.character(polish_dimensions("8to(pienempi)")$gatherings), "8vo") 
-  expect_equal(as.character(polish_dimensions("12top.")$gatherings), "12mo")
-  expect_equal(as.character(polish_dimensions("kuv. ;;8to+")$gatherings), "8long")
-  expect_equal(as.character(polish_dimensions("kuv. ;;4to")$gatherings), "4to")
-  expect_equal(as.character(polish_dimensions("4to(21 x 26 cm)")$gatherings), "4to")
-
-  # Larger one is height unless obl. is defined
-  expect_equal(as.character(polish_dimensions("4to(21 x 26 cm)")$height), "26")
-  expect_equal(as.character(polish_dimensions("4to(21 x 26 cm)")$width), "21")
-  expect_equal(as.character(polish_dimensions("16to[7.5x5.5 cm].")$width), "5.5")
-  expect_equal(as.character(polish_dimensions("16to[7.5x5.5 cm].")$height), "7.5")    
-
-  expect_equal(as.character(polish_dimensions("4togrand papier." )$gatherings), "4long")      
-  expect_equal(as.character(polish_dimensions("4to(oblong)")$gatherings), "4long")
-
+  f <- "~/GitHub/bibliographica/tests/dimension_polish_tests_fennica.csv"
+  synonymes <- read.csv(file=f, header=TRUE, sep="\t", encoding="UTF-8")
+  for (i in 1:nrow(synonymes)) {
+    test_clause <- as.character(synonymes$clause[i])
+    test_result <- as.character(synonymes$result[i])
+    if (is.na(test_result)) {test_result <- "NA"}
+    test_field <- as.character(synonymes$field[i])
+    test_fill <- as.character((synonymes$fill[i]))
+    
+    # This appears to be completely irrelevant?
+    if (test_fill=="TRUE") {test_fill <- TRUE} else {test_fill <- FALSE}
+    if ((is.na(test_field)) || (test_field =="")) {test_field <- "gatherings"}
+    if (is.na(synonymes$expected[i]) || (synonymes$expected[i] == "equal")) {
+      expect_equal(as.character(polish_dimensions(test_clause)[[test_field]]), test_result)
+    }
+  }
   # Take care later
   #expect_equal(as.character(polish_dimensions("kotelo 48 x 56 cm.")$gatherings), 4)
   #expect_equal(as.character(polish_dimensions("obl.1to[-1/2to].")$gatherings), 4)
@@ -153,12 +66,7 @@ test_that("fennica dimensions", {
   #expect_equal(as.character(polish_dimensions("obl.1to[1/2to].")$gatherings), 4)
   #expect_equal(as.character(polish_dimensions("obl.1to[1/2to.")$gatherings), 4)
   #expect_equal(as.character(polish_dimensions("obl.1to1/2to].")$gatherings), 4)
-
-  expect_equal(as.character(polish_dimensions("o.", fill = TRUE)$gatherings), "NA")
-  expect_equal(as.character(polish_dimensions("[?to.", fill = TRUE)$gatherings), "NA")
-  expect_equal(as.character(polish_dimensions("?to", fill = TRUE)$gatherings), "NA")
-  expect_equal(as.character(polish_dimensions("Koko vaihtelee.", fill = TRUE)$gatherings), "NA")
-
+  
 })
 
 
