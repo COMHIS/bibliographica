b35a60dea2f7bb4cc5b1bd027a2e369de5d56cdc
antagomir <leo.lahti@iki.fi>
2016-07-06 20:01:41 +0200

    ok

--- R/titlecount_timeline.R
+++ R/titlecount_timeline.R
@@ -1,7 +1,7 @@
 #' @title Title Count Timeline
 #' @description Compare title (document) count among selected groups.
 #' @param x data frame
-#' @param group_by Field indicating the groups to compare
+#' @param group Field indicating the groups to compare
 #' @param nmin Include only entries with at least nmin occurrences
 #' @param mode "n" (number of entries) or "percentage" (fraction of entries)
 #' @return List:
@@ -14,37 +14,37 @@
 #' @references See citation("bibliographica")
 #' @examples \dontrun{titlecount_timeline(df, "gatherings")}
 #' @keywords utilities
-titlecount_timeline <- function (x, group_by, nmin = 0, mode = "n") {
+titlecount_timeline <- function (x, group, nmin = 0, mode = "n") {
 
   publication_decade <- NULL
+  x <- x[, c("publication_decade", group)]
 
-  x$group_by <- x[[group_by]]
+  x$group <- x[[group]]
   
-  df2 <- x %>% filter(!is.na(group_by)) %>% group_by(publication_decade, group_by) %>%
+  df2 <- x %>% filter(!is.na(group)) %>% group_by(publication_decade, group) %>%
      summarize(n = n())
 
   # Remove entries with too few occurrences
   df2 <- df2 %>% filter(!is.na(publication_decade) &
-    group_by %in% setdiff(names(which(table(df2$group_by) >= nmin)), "NA"))
-  #df2 <- df2 %>% dplyr::filter(group_by %in% setdiff(names(which(table(df2$group_by) >= nmin)), "NA"))  
-  df2$group_by <- droplevels(df2$group_by)
+    group %in% setdiff(names(which(table(df2$group) >= nmin)), "NA"))
+  df2$group <- droplevels(df2$group)
   
 
   # Add percentages
   df3 = spread(df2, "publication_decade", "n", fill = 0)
   df3[, -1] = 100 * apply(df3[, -1], 2, function (x) {x/sum(x)})
-  df3 = gather(df3, "group_by")
-  colnames(df3) = c("group_by", "publication_decade", "percentage")
+  df3 = gather(df3, group)
+  colnames(df3) = c("group", "publication_decade", "percentage")
   df3$publication_decade = as.numeric(df3$publication_decade)
 
   # Combine counts and percentages
   dfs = dplyr::full_join(df2, df3)
   dfs$mode = dfs[[mode]]
 
-  p <- ggplot(dfs, aes(y = mode, x = publication_decade, shape = group_by, linetype = group_by)) +
+  p <- ggplot(dfs, aes(y = mode, x = publication_decade, shape = group, linetype = group)) +
      geom_point(size = 4) +
-     geom_line(aes(color = group_by), size = 1) +          
-     ggtitle(paste("Title count in time by ", group_by)) +
+     geom_line(aes(color = group), size = 1) +          
+     ggtitle(paste("Title count in time by ", group)) +
      xlab("Year") + ylab(paste("Frequency (", mode, ")", sep = "")) +
      guides(linetype = guide_legend(keywidth = 5), shape = guide_legend(keywidth = 5)) +
      ggtitle("Title count timeline") 
