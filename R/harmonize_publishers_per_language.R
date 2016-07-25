56d2b3575a43a179e2978692b9d4a31eae79d8a6
antagomir <leo.lahti@iki.fi>
2016-07-25 23:47:08 +0300

    ok

--- R/harmonize_publishers_per_language.R
+++ R/harmonize_publishers_per_language.R
@@ -4,13 +4,13 @@ harmonize_publishers_per_language <- function (q, languages) {
 
   # Used to the first thing to do in this function
   for (language in languages) {
-    if (language=="swedish") {
+    if (language == "swedish") {
       f <- system.file("extdata/sv_publisher.csv", package="bibliographica")
-    } else if (language=="english") {
+    } else if (language == "english") {
       f <- system.file("extdata/en_publisher.csv", package="bibliographica")
-    } else if (language=="finnish") {
+    } else if (language == "finnish") {
       f <- system.file("extdata/fi_publisher.csv", package="bibliographica")
-    } else if (language=="latin") {
+    } else if (language == "latin") {
       f <- system.file("extdata/lat_publisher.csv", package="bibliographica")
     } else {
       warning(paste0("Unknown language in languages: ", language))
@@ -18,7 +18,7 @@ harmonize_publishers_per_language <- function (q, languages) {
 
     synonyms <- read.csv(f, sep = "\t", fileEncoding = "UTF-8")
 
-    q <- map(q, synonyms, mode="recursive")
+    q <- map(q, synonyms, mode = "recursive")
 
   }
 
