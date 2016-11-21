dfd861762aca5454f623581a78baa7c7c75f3efe
antagomir <leo.lahti@iki.fi>
2016-11-21 20:31:59 +0100

    Added publication frequency polish for english language. Unit tests still todo

--- R/polish_publication_frequency.R
+++ R/polish_publication_frequency.R
@@ -11,9 +11,12 @@ polish_publication_frequency <- function(x) {
   # Remove periods  
   x <- condense_spaces(tolower(gsub("\\.$", "", x)))
   x <- gsub("^ca ", "", x)
+  x <- gsub("\\?", " ", x)
+  x <- gsub("'", " ", x)    
   x <- gsub(" */ *", "/", x)
   x <- gsub("^[0-9]+ s$", "", x)
-  
+  x <- condense_spaces(x)
+
   f <- system.file("extdata/replace_special_chars.csv",
                 package = "bibliographica")
   spechars <- suppressWarnings(read_mapping(f, sep = ";", mode = "table", include.lowercase = TRUE))
@@ -36,7 +39,8 @@ polish_publication_frequencies <- function (x) {
   tmps <- list()
   tmps[["Finnish"]] <- suppressWarnings(polish_publication_frequency_finnish(x))
   tmps[["Swedish"]] <- suppressWarnings(polish_publication_frequency_swedish(x))
-  tmp <- tmps[[names(which.min(colSums(sapply(tmps, function (tmp) {is.na(tmp)}))))]]
+  tmps[["English"]] <- suppressWarnings(polish_publication_frequency_english(x))  
+  tmp <- tmps[[names(which.min(sapply(tmps, function (tmp) {sum(is.na(tmp))})))]]
 
   # Convert all units to years
   unityears <- tmp$unit
@@ -59,6 +63,401 @@ polish_publication_frequencies <- function (x) {
 }
 
 
+
+
+
+#' @title Polish Publication Frequency English
+#' @description Harmonize publication frequencies for English data.
+#' @param x publication frequency field (a vector) 
+#' @author Leo Lahti \email{leo.lahti@@iki.fi}
+#' @references See citation("bibliographica")
+#' @examples \dontrun{df <- polish_publication_frequency_english("weekly")}
+#' @keywords utilities
+polish_publication_frequency_english <- function(x) {
+
+  # TODO add to CSV list rather than mixed in the code here				     
+  x <- gsub("issued several times a week, frequency of issue varies", "3 per week", x)
+  x <- gsub("suday", "sunday", x)
+  x <- gsub("colleced", "collected", x)
+  x <- gsub(" and index$", "", x)    
+  x <- gsub(", when parliament is in session", "", x)
+  x <- gsub("\\(when parliament is in session\\)", "", x)
+  x <- gsub("\\(when parliament is sitting\\)", "", x)
+  x <- gsub("\\(while parliament is sitting\\)", "", x)  
+  x <- gsub("\\(published according to sitting dates of parliament\\)", "", x)
+  x <- gsub("\\(during the sitting dates of parliament\\)", "", x)
+  x <- gsub("\\(during the sitting of parliament\\)", "", x)      
+  x <- gsub(", with irregular special issues", "", x)  
+  x <- gsub(" \\(collected [a-z]+\\)", "", x)
+  x <- gsub(" \\(during the racing season\\)", "", x)
+  x <- gsub(" \\(collected issues for [0-9]+\\)", "", x)
+  x <- gsub(" \\(collected annually, [0-9]+-[0-9]+\\)", "", x)  
+  x <- gsub(" \\(collected [0-9]+ times a [a-z]+\\)", "", x)
+  x <- gsub(" \\(collected [a-z]+ [a-z]+\\)", "", x)
+  x <- gsub(" \\(collected [a-z]+ [0-9]+ issues\\)", "", x)
+  x <- gsub(" \\(compiled issues\\)", "", x)
+  x <- gsub(" \\(compilation\\)", "", x)      
+  x <- gsub(" \\(collected [a-z]+\\)", "", x)    
+  x <- gsub(" \\(cumulati*ve\\)", "", x)
+  x <- gsub(" \\(cumulated\\)", "", x)
+  x <- gsub(" \\(with general title page\\)", "", x)  
+  x <- gsub(" \\(cumulates monthly issues\\)", "", x)
+  x <- gsub(" \\(cumulated every [a-z]+ numbers\\)", "", x)  
+  x <- gsub(" \\(with [a-z]+ cumulation\\)", "", x)
+  x <- gsub(", with annual or semiannual cumulation", "", x)
+  x <- gsub(", with annual cumulation and indexes", "", x)  
+  x <- gsub(", with [0-9]+-* *[a-z]+ cumulations*", "", x)
+  x <- gsub(", with [a-z]+ cumulations*", "", x)  
+  x <- gsub(", with cumulation in [0-9]+ volumes*", "", x)  
+  x <- gsub(", with [a-z]+ [a-z]+ cumulations*", "", x)  
+  x <- gsub(" \\(with [a-z]+ cumulations*\\)", "", x)  
+  x <- gsub(" \\(cumulation\\)", "", x)
+  x <- gsub(" \\(on mondays\\)", "", x)
+  x <- gsub(" \\(on tuesdays\\)", "", x)    
+  x <- gsub(" \\(on wednesdays\\)", "", x)
+  x <- gsub(" \\(on thursdays\\)", "", x)
+  x <- gsub(" \\(on fridays\\)", "", x)
+  x <- gsub(" \\(on saturdays\\)", "", x)          
+  x <- gsub(" \\(on sundays\\)", "", x)
+  x <- gsub(" \\(mondays\\)", "", x)
+  x <- gsub(" \\(tuesdays\\)", "", x)
+  x <- gsub(" \\(tues\\)", "", x)      
+  x <- gsub(" \\(wednesdays\\)", "", x)
+  x <- gsub(" \\(thursdays\\)", "", x)
+  x <- gsub(" \\(thurs\\.*\\)", "", x)  
+  x <- gsub(" \\(fridays\\)", "", x)
+  x <- gsub(" \\(saturdays\\)", "", x)          
+  x <- gsub(" \\(sundays\\)", "", x)
+  x <- gsub(" \\(w?th some variation\\)", "", x)
+  x <- gsub(", with occasional supplements", "", x)
+  x <- gsub("bi-", "bi", x)
+  x <- gsub("semi-", "semi", x)
+  x <- gsub("annually", "annual", x)
+  x <- gsub("during the law terms", " ", x)
+  x <- gsub(" \\[[0-9]+\\]", "", x)
+  x <- condense_spaces(x)
+  
+  freq <- rep(NA, length = length(x))
+  unit <- rep(NA, length = length(x))  
+
+  # English
+  f <- system.file("extdata/numbers_english.csv", package = "bibliographica")
+  #f <- "~/Rpackages/bibliographica/inst/extdata/numbers_english.csv"
+  char2num <- read_mapping(f, sep = ",", mode = "table", from = "character", to = "numeric")
+  x <- map(x, synonymes = char2num, from = "character", to = "numeric", mode = "match")
+
+  # 18 issues per year
+  inds <- grep("^[0-9]+ issues ", x)
+  if (length(inds)>0) {
+    x[inds] <- gsub(" issues", "", x[inds])
+  }
+
+  # daily
+  inds <- grep("^daily", x)
+  if (length(inds)>0) {
+    freq[inds] <- 1
+    unit[inds] <- "day"
+    x[inds] <- NA # handled
+  }
+
+  # 2 daily
+  inds <- grep("^[0-9]+ daily$", x)
+  if (length(inds)>0) {
+    freq[inds] <- unlist(strsplit(x[inds], " "), use.names = F)[[1]]
+    unit[inds] <- "day"
+    x[inds] <- NA # handled    
+  }
+
+  # daily, except sunday -> 6/week
+  inds <- grep("^daily,* except sunday$", x)
+  if (length(inds)>0) {
+    freq[inds] <- 6
+    unit[inds] <- "week"
+    x[inds] <- NA # handled    
+  }
+
+  # every other day
+  inds <- grep("^every other day$", x)
+  if (length(inds)>0) {
+    freq[inds] <- 1/2
+    unit[inds] <- "day"
+    x[inds] <- NA # handled    
+  }
+
+  # daily except weekends
+  inds <- grep("^daily \\(except weekends\\)$", x)
+  if (length(inds)>0) {
+    freq[inds] <- 5
+    unit[inds] <- "week"
+    x[inds] <- NA # handled    
+  }
+
+  # daily except weekends
+  inds <- grep("^daily \\(except [a-z]+\\. & [a-z]+\\.\\)$", x)
+  if (length(inds)>0) {
+    freq[inds] <- 5
+    unit[inds] <- "week"
+    x[inds] <- NA # handled    
+  }
+
+  # daily except sunday
+  inds <- grep("^daily \\(except sundays*\\)$", x)
+  if (length(inds)>0) {
+    freq[inds] <- 6
+    unit[inds] <- "week"
+    x[inds] <- NA # handled    
+  }
+
+  # daily (except sun.)
+  inds <- grep("^daily \\(except [a-z]+\\.\\)$", x)
+  if (length(inds)>0) {
+    freq[inds] <- 6
+    unit[inds] <- "week"
+    x[inds] <- NA # handled    
+  }
+
+  # weekly
+  inds <- grep("^weekly", x)
+  if (length(inds)>0) {
+    freq[inds] <- 1
+    unit[inds] <- "week"
+    x[inds] <- NA # handled    
+  }
+
+  # semiweekly
+  inds <- grep("^semiweekly$", x)
+  if (length(inds)>0) {
+    freq[inds] <- 2
+    unit[inds] <- "week"
+    x[inds] <- NA # handled    
+  }
+
+  # semiweekly (tuesday and friday) 
+  inds <- grep("^semiweekly \\([a-z]+ and [a-z]+\\)$", x)
+  if (length(inds)>0) {
+    freq[inds] <- 2
+    unit[inds] <- "week"
+    x[inds] <- NA # handled    
+  }
+
+  # biweekly
+  inds <- grep("^biweekly", x)
+  if (length(inds)>0) {
+    freq[inds] <- 2
+    unit[inds] <- "week"
+    x[inds] <- NA # handled    
+  }
+
+  # triweekly
+  inds <- grep("^triweekly$", x)
+  if (length(inds)>0) {
+    freq[inds] <- 3
+    unit[inds] <- "week"
+    x[inds] <- NA # handled    
+  }
+
+  # monthly
+  inds <- grep("^monthly$", x)
+  if (length(inds)>0) {
+    freq[inds] <- 1
+    unit[inds] <- "month"
+    x[inds] <- NA # handled    
+  }
+
+  # bimonthly
+  inds <- grep("^bimonthly$", x)
+  if (length(inds)>0) {
+    freq[inds] <- 1/2
+    unit[inds] <- "month"
+    x[inds] <- NA # handled    
+  }
+
+  # semimonthly
+  inds <- grep("^semimonthly$", x)
+  if (length(inds)>0) {
+    freq[inds] <- 1/2
+    unit[inds] <- "month"
+    x[inds] <- NA # handled    
+  }
+
+  # quarterly
+  inds <- grep("^quarterly", x)
+  if (length(inds)>0) {
+    freq[inds] <- 4
+    unit[inds] <- "year"
+    x[inds] <- NA # handled    
+  }
+
+  # annual
+  inds <- grep("^annual *\\(*", x)
+  if (length(inds)>0) {
+    freq[inds] <- 1
+    unit[inds] <- "year"
+    x[inds] <- NA # handled    
+  }
+
+  # semiannual
+  inds <- grep("^semiannual", x)
+  if (length(inds)>0) {
+    freq[inds] <- 2
+    unit[inds] <- "year"
+    x[inds] <- NA # handled    
+  }
+
+  # biennial
+  inds <- grep("^biennial$", x)
+  if (length(inds)>0) {
+    freq[inds] <- 1/2
+    unit[inds] <- "year"
+    x[inds] <- NA # handled    
+  }
+
+  # two or three times a year
+  inds <- grep("^[0-9]+ or [0-9]+ times a [a-z]+$", x)
+  if (length(inds)>0) {
+    spl <- strsplit(x[inds], " ")
+    freq[inds] <- mean(as.numeric(sapply(spl, function (xi) {xi[[1]]})), as.numeric(sapply(spl, function (xi) {xi[[3]]})))
+    unit[inds] <- sapply(spl, function (xi) {xi[[6]]})
+    x[inds] <- NA # handled    
+  }
+
+  # two or three times a year
+  inds <- grep("^[0-9]+ to [0-9]+ times a [a-z]+$", x)
+  if (length(inds)>0) {
+    spl <- strsplit(x[inds], " ")
+    freq[inds] <- mean(as.numeric(sapply(spl, function (xi) {xi[[1]]})), as.numeric(sapply(spl, function (xi) {xi[[3]]})))
+    unit[inds] <- sapply(spl, function (xi) {xi[[6]]})
+    x[inds] <- NA # handled    
+  }
+
+  # three times a year
+  inds <- grep("^[0-9]+ times a [a-z]+", x)
+  if (length(inds)>0) {
+    x[inds] <- gsub(" times a ", " ", x[inds])
+    spl <- strsplit(x[inds], " ")
+    freq[inds] <- as.numeric(sapply(spl, function (xi) {xi[[1]]}))
+    unit[inds] <- sapply(spl, function (xi) {xi[[2]]})
+    x[inds] <- NA # handled    
+  }
+
+  # three times weekly
+  inds <- grep("^[0-9]+ times [a-z]+$", x)
+  if (length(inds)>0) {
+    spl <- strsplit(x[inds], " ")
+    freq[inds] <- as.numeric(sapply(spl, function (xi) {xi[[1]]}))
+    unit[inds] <- sapply(spl, function (xi) {xi[[3]]})
+    x[inds] <- NA # handled    
+  }
+
+  # three per year
+  inds <- grep("^[0-9]+ per [a-z]+$", x)
+  if (length(inds)>0) {
+    spl <- strsplit(x[inds], " ")
+    freq[inds] <- as.numeric(sapply(spl, function (xi) {xi[[1]]}))
+    unit[inds] <- sapply(spl, function (xi) {xi[[3]]})
+    x[inds] <- NA # handled    
+  }
+
+  # three times per year
+  inds <- grep("^[0-9]+ times per [a-z]+$", x)
+  if (length(inds)>0) {
+    x[inds] <- gsub(" times per ", " ", x[inds])
+    spl <- strsplit(x[inds], " ")
+    freq[inds] <- as.numeric(sapply(spl, function (xi) {xi[[1]]}))
+    unit[inds] <- sapply(spl, function (xi) {xi[[2]]})
+    x[inds] <- NA # handled    
+  }
+
+  # 4 issues in 6 months
+  inds <- grep("^[0-9]+ issues in [0-9]+ [a-z]+$", x)
+  if (length(inds)>0) {
+    spl <- strsplit(x[inds], " ")
+    n1 <- as.numeric(sapply(spl, function (xi) {xi[[1]]}))
+    n2 <- as.numeric(sapply(spl, function (xi) {xi[[4]]}))
+    u <- sapply(spl, function (xi) {xi[[5]]})
+    freq[inds] <- n1/n2
+    unit[inds] <- u
+    x[inds] <- NA # handled    
+  }
+
+  # 4 in 6 months
+  inds <- grep("^[0-9]+ in [0-9]+ [a-z]+$", x)
+  if (length(inds)>0) {
+    spl <- strsplit(x[inds], " ")
+    n1 <- as.numeric(sapply(spl, function (xi) {xi[[1]]}))
+    n2 <- as.numeric(sapply(spl, function (xi) {xi[[3]]}))
+    u <- sapply(spl, function (xi) {xi[[4]]})
+    freq[inds] <- n1/n2
+    unit[inds] <- u
+    x[inds] <- NA # handled    
+  }
+
+  # 1 during a 6teen-month period
+  inds <- grep("^[0-9]+ during a [0-9]+-[a-z]+ period", x)
+  if (length(inds)>0) {
+    x[inds] <- gsub("-", " ", x[inds])
+    spl <- strsplit(x[inds], " ")
+    n1 <- as.numeric(sapply(spl, function (xi) {xi[[1]]}))
+    n2 <- as.numeric(sapply(spl, function (xi) {xi[[4]]}))
+    u <- sapply(spl, function (xi) {xi[[5]]})
+    freq[inds] <- n1/n2
+    unit[inds] <- u
+    x[inds] <- NA # handled    
+  }
+
+  # every five days
+  inds <- grep("^every [0-9]+ [a-z]+$", x)
+  if (length(inds)>0) {
+    x[inds] <- gsub("^every ", "", x[inds])
+    spl <- strsplit(x[inds], " ")
+    freq[inds] <- 1/as.numeric(sapply(spl, function (xi) {xi[[1]]}))
+    unit[inds] <- sapply(spl, function (xi) {xi[[2]]})
+    x[inds] <- NA # handled    
+  }
+
+  # twice every three weeks
+  inds <- grep("^[0-9]+ every [0-9]+ [a-z]+$", x)
+  if (length(inds)>0) {
+    spl <- strsplit(x[inds], " ")
+
+    freq[inds] <- as.numeric(sapply(spl, function (xi) {xi[[1]]}))/as.numeric(sapply(spl, function (xi) {xi[[3]]}))
+    unit[inds] <- sapply(spl, function (xi) {xi[[4]]})
+    x[inds] <- NA # handled    
+  }
+
+  # semiannual
+  inds <- grep("^semiannual$", x)
+  if (length(inds)>0) {
+    freq[inds] <- 2
+    unit[inds] <- "year"
+    x[inds] <- NA # handled    
+  }
+
+  # irregular
+  inds <- c(
+    which(x == "irregular"),
+    which(x == "unknown"),
+    which(x == "frequency unknown"),        
+    which(x == "frequency irregular"),
+    which(x == "no determinable frequency")
+  )
+  if (length(inds)>0) {
+    freq[inds] <- NA
+    unit[inds] <- "Irregular"
+    x[inds] <- NA # handled        
+  }
+
+  if (is.null(x) || is.na(x)) {
+    # skip
+  }
+
+  # Translate units (weeks -> week; years -> year etc)
+  unit <- gsub("s$", "", unit)
+
+  # orig = x, 
+  data.frame(unit = unit, freq = freq)
+
+}
 
 
 
