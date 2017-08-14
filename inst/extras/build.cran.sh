4104a96b4d6844e021a1fab7e8aec00a79b50e1b
Leo Lahti <leo.lahti@utu.fi>
2017-08-14 13:00:42 +0200

    version update

--- inst/extras/build.cran.sh
+++ inst/extras/build.cran.sh
@@ -5,7 +5,7 @@
 # http://r-pkgs.had.co.nz/man.html
 
 #/home/lei/bin/R CMD BATCH document.R
-/home/lei/bin/R-3.4.0/bin/R CMD build ../../ --no-build-vignettes #--no-tests #--no-examples 
+/home/lei/bin/R CMD build ../../ --no-build-vignettes #--no-tests #--no-examples 
 #/home/lei/bin/R-3.4.0/bin/R CMD check --as-cran bibliographica_0.1.22.tar.gz
-/home/lei/bin/R-3.4.0/bin/R CMD check bibliographica_0.2.31.tar.gz --no-build-vignettes --no-tests #--no-examples 
-/home/lei/bin/R-3.4.0/bin/R CMD INSTALL bibliographica_0.2.31.tar.gz
+/home/lei/bin/R CMD check bibliographica_0.2.32.tar.gz --no-build-vignettes --no-tests #--no-examples 
+/home/lei/bin/R CMD INSTALL bibliographica_0.2.32.tar.gz
