b32aa34d6419006ad7ef2f806c04efb45388e19a
antagomir <leo.lahti@iki.fi>
2017-12-12 18:51:31 +0200

    TODO list

--- inst/extdata/preprocessing.R
+++ inst/extdata/preprocessing.R
@@ -5,8 +5,8 @@ preprocess_data <- function(preprocessing.data, df.orig, languages, mc.cores = 1
   # mc.cores required by polish_all()
 
   df.preprocessed <- preprocessing.data$df.preprocessed
-  update.fields <-   preprocessing.data$update.fields
-  conversions <-     preprocessing.data$conversions
+  update.fields   <- preprocessing.data$update.fields
+  conversions     <- preprocessing.data$conversions
 
   message("Preprocess selected original fields")
   
