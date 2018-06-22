b4c53b763fba9690d9b88763e5e22bfd11e5c8fa
antagomir <leo.lahti@iki.fi>
2018-06-22 22:44:48 +0200

    ok

--- inst/extras/build.cran.R344.sh
+++ inst/extras/build.cran.R344.sh
@@ -5,5 +5,5 @@
 # http://r-pkgs.had.co.nz/man.html
 
 ~/bin/R-3.4.4/bin/R CMD build ../../ --no-build-vignettes #--no-tests #--no-examples 
-~/bin/R-3.4.4/bin/R CMD check bibliographica_0.2.43.tar.gz --no-build-vignettes --no-tests #--no-examples 
-~/bin/R-3.4.4/bin/R CMD INSTALL bibliographica_0.2.43.tar.gz
+~/bin/R-3.4.4/bin/R CMD check bibliographica_0.2.44.tar.gz --no-build-vignettes --no-tests #--no-examples 
+~/bin/R-3.4.4/bin/R CMD INSTALL bibliographica_0.2.44.tar.gz
