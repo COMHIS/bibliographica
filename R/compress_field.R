87242a6a21f237c9ff759015ccf640f2a9b50f4f
Leo <leo.lahti@iki.fi>
2019-02-19 13:25:54 +0200

    ok

--- R/compress_field.R
+++ R/compress_field.R
@@ -1,24 +1,40 @@
 #' @title Compress Field into Fewer Categories
 #' @description Merge the less common terms into a single group
 #' @param x Vector or factor.
-#' @param keep Number of top entries to include; or a character vector 
+#' @param topn Number of top entries to include; or a character vector 
+#' @param min.freq Minimum frequency for the included groups.
 #' @param rest Name to be used for the remaining entries
 #' @return Compressed version of the input.
 #' @export
+#' @details With the min.freq argument, groups that have less items
+#'          that this will be merged.
 #' @author Leo Lahti \email{leo.lahti@@iki.fi}
 #' @references See citation("bibliographica")
 #' @examples
 #'   p <- compress_field(c("A", "A", "A", "B","B","C", "D"),
-#'          keep = 2, rest = "Other")
+#'          topn = 2, rest = "Other")
 #'   p <- compress_field(c("A", "A", "A", "B","B","C", "D"),
-#'          keep = c("A", "B"), rest = "Other")
+#'          topn = c("A", "B"), rest = "Other")
+#'   p <- compress_field(c("A", "A", "A", "B","B","C", "D"),
+#'          min.freq = 2, rest = "Other")
 #' @keywords utilities
-compress_field <- function (x, keep, rest = "Other") {
+compress_field <- function (x, topn, min.freq = NULL, rest = "Other") {
 
-  if (is.numeric(keep)) {	         
-    top <- names(top(x, n = keep, na.rm = TRUE))
+  # keep was replace with topn
+
+  if (!is.null(topn) && !is.null(min.freq)) {
+    stop("Both topn and min.freq given; min.freq will be used in this
+          case.")
+  }
+
+  if (!is.null(min.freq)) {
+    topn <- names(which(table(x) >= min.freq))
+  }
+  
+  if (is.numeric(topn)) {	         
+    top <- names(top(x, n = topn, na.rm = TRUE))
   } else {
-    top <- keep
+    top <- topn
   }
   x <- as.character(x)
   x[which(!x %in% top)] <- rest
@@ -26,3 +42,4 @@ compress_field <- function (x, keep, rest = "Other") {
   x
   
 }
+
