1cb4adb1b2bc8d174d82e7ee1141316dabeeae86
antagomir <leo.lahti@iki.fi>
2019-06-10 22:29:01 +0300

    added function to retrieve gatherings size

--- inst/extras/build.cran2.sh
+++
@@ -1,9 +0,0 @@
-# For info on Travis R scripts, see
-# http://jtleek.com/protocols/travis_bioc_devel/
-
-# Roxygen tips:
-# http://r-pkgs.had.co.nz/man.html
-
-~/bin/R-3.5.1/bin/R CMD build ../../ --no-build-vignettes #--no-tests #--no-examples 
-~/bin/R-3.5.1/bin/R CMD check bibliographica_0.2.47.tar.gz --no-build-vignettes --no-tests #--no-examples 
-~/bin/R-3.5.1/bin/R CMD INSTALL bibliographica_0.2.47.tar.gz
