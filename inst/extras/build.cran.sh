819ebd50e041667b9c3adb50bdaef8655f38e2f9
antagomir <leo.lahti@iki.fi>
2021-08-09 14:31:54 +0300

    ok

--- inst/extras/build.cran.sh
+++ inst/extras/build.cran.sh
@@ -4,6 +4,6 @@
 # Roxygen tips:
 # http://r-pkgs.had.co.nz/man.html
 
-/usr/bin/R CMD build ../../ --no-build-vignettes #--no-tests #--no-examples 
-/usr/bin/R CMD check bibliographica_0.2.61.tar.gz --no-build-vignettes --no-tests #--no-examples 
-/usr/bin/R CMD INSTALL bibliographica_0.2.61.tar.gz
+~/bin/R-4.1.0/bin/R CMD build ../../ --no-build-vignettes #--no-tests #--no-examples 
+~/bin/R-4.1.0/bin/R CMD check bibliographica_0.2.61.tar.gz --no-build-vignettes --no-tests #--no-examples 
+~/bin/R-4.1.0/bin/R CMD INSTALL bibliographica_0.2.61.tar.gz
